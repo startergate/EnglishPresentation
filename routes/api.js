f14823f3786438361db0da2c2481d508d6625864
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-10-23 16:04:06 +0900

    [UPDATE] Finish Back-end

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const mysql = require("mysql");
+const setting = require('../modules/db.setting');
 const router = express.Router();
+
+let conn = mysql.createConnection({
+  host     : setting.host,
+  user     : setting.user,
+  password : setting.password,
+  database : setting.database
+});
 
 /* GET home page. */
 router.put('/:id', (req, res, next) => {
-  // TODO: MySQL 통신
+  conn.query(`UPDATE data SET madisonSquare='${conn.escape(req.body.madisonSquare)}', restaurant='${conn.escape(req.body.restaurant)}', stores = '${conn.escape(req.body.stores)}', church = '${conn.escape(req.body.church)}' WHERE id = ${conn.escape(req.params.id)}`);
+  res.sendStatus(200);
 });
 
 router.get('/', (req, res, next) => {
-
+  conn.query("SELECT * FROM data", (err, result, fields) => {
+    res.send(result);
+  })
 });
 
 module.exports = router;
